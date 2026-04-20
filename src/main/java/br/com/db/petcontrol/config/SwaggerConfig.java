99a11ce9b58683fccde54235a4641a89d9045524
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-04-20 15:10:39 -0300

    [PETCONTROL-000] chore: configurar docker, banco de dados, swagger e JaCoCo

---
+++ src/main/java/br/com/db/petcontrol/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package br.com.db.petcontrol.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PetControl API",
+                version = "1.0",
+                summary = "API para gerenciamento de animais."
+        )
+)
+public class SwaggerConfig {
+}
