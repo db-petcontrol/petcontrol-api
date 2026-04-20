7b507664660e8166b0d803b2536b7fa4f95c7368
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-04-20 13:58:35 -0300

    init: first commit

---
+++ src/main/java/br/com/db/petcontrol/PetcontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.db.petcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/db/petcontrol/PetcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.db.petcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
